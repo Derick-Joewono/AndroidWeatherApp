b59964209feb9c620d87d5f9d14a659d79d1780e
Morenozio <morenopatrizio1010@gmail.com>
2025-12-14 17:57:46 +0700

    Implemented UI and get started page

--- app/src/main/java/com/example/androidweatherapp/api/RetrofitClient.java
+++ app/src/main/java/com/example/androidweatherapp/api/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+    private static final String BASE_URL = "https://api.openweathermap.org/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
